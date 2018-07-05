c12c35fb2cd55f76be18d8a53d9776539ad839de
MKwiatosz <you@example.com>
2018-07-05 15:04:32 +0200

    [Added sentiment analysis through all tweets]

--- Python/Python_/2/main.py
+++ Python/Python_/2/main.py
@@ -1,20 +1,42 @@
+# Dependencies
 import tweepy
 from textblob import TextBlob
+import csv
 
+# Consumer keys to twitter
 consumer_key = 'hlMP7rdxx8dd3BXKdy03Gb55v'
 consumer_secret = 'irTGW16aT5SWnErb6G2ETuaA85M7MIxdrLc1B181OcTv1VtrEx'
 
+# Acces tokens to twitter
 acces_token = '1011299037826469892-Ur5ZNshcmxanLUfO2xoDoobXLWyLnz'
 acces_token_secret = 'PZxlecb93ztHKByhTnzKuxCt0UwfIS5KX4ksM3vDX8z6z'
 
+# Authorization to log in into twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(acces_token,acces_token_secret)
 
+# API instance
 api = tweepy.API(auth)
 
-public_tweets = api.search('Trump')
+# Searching "word"
+public_tweets = api.search('Love')
 
+# Definied threshold to specified tweet
+def get_label(analysis, threshold=0):
+    if analysis.sentiment[0] > threshold:
+        return 'Positive'
+    elif analysis.sentiment[0] == 0:
+        return 'Neutral'
+    else:
+        return 'Negative'
+
+# Make csv file
+csvFile = open('tweets.csv','w')
+csvWriter = csv.writer(csvFile)
+
+# Loop which is writing tweets into csv and defining what attitude have sentence
 for tweet in public_tweets:
-    print(tweet.text)
     analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'), get_label(analysis)])
+    print (tweet.created_at, tweet.text)
+csvFile.close()
